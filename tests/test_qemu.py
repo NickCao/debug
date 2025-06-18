d4c91d23d6ddaf171f9dc621b6a582e5b52b9a4b
Nick Cao <nickcao@nichi.co>
2025-06-18 13:46:38 -0400

    Fixup

--- tests/test_qemu.py
+++ tests/test_qemu.py
@@ -1,5 +1,4 @@
 import sys
-from pathlib import Path
 
 from opendal import Operator
 
@@ -7,43 +6,25 @@ from jumpstarter_testing.pytest import JumpstarterTest
 
 
 class TestQemu(JumpstarterTest):
-    def test_driver_qemu(tmp_path, ovmf, client):
-        arch = "x86_64"
-
-        qemu = client
-
-        cached_image = (
-            Path(__file__).parent.parent
-            / "images"
-            / f"Fedora-Cloud-Base-Generic-41-1.4.{arch}.qcow2"
-        )
-
-        if cached_image.exists():
-            qemu.flasher.flash(cached_image.resolve())
-        else:
-            qemu.flasher.flash(
-                f"pub/fedora/linux/releases/41/Cloud/{arch}/images/Fedora-Cloud-Base-Generic-41-1.4.{arch}.qcow2",
+    def test_driver_qemu(tmp_path, client):
+        with client.log_stream():
+            client.storage.flash(
+                "pub/fedora/linux/releases/41/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2",
                 operator=Operator(
                     "http", endpoint="https://download.fedoraproject.org"
                 ),
             )
 
-        qemu.power.on()
+            client.power.cycle()
 
-        with qemu.novnc() as _:
-            pass
+            with client.serial.pexpect() as p:
+                p.logfile = sys.stdout.buffer
+                p.expect_exact("login:", timeout=600)
+                p.sendline("jumpstarter")
+                p.expect_exact("Password:")
+                p.sendline("password")
+                p.expect_exact(" ~]$")
+                p.sendline("sudo setenforce 0")
+                p.expect_exact(" ~]$")
 
-        with qemu.console.pexpect() as p:
-            p.logfile = sys.stdout.buffer
-            p.expect_exact("login:", timeout=600)
-            p.sendline("jumpstarter")
-            p.expect_exact("Password:")
-            p.sendline("password")
-            p.expect_exact(" ~]$")
-            p.sendline("sudo setenforce 0")
-            p.expect_exact(" ~]$")
-
-        with qemu.shell() as s:
-            assert s.run("uname -r").stdout.strip() == f"6.11.4-301.fc41.{arch}"
-
-        qemu.power.off()
+            client.power.off()
